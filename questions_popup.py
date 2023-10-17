2ade296cf3872f34301594fed4440c3715e97062
archie3DXR <archie.majumdar@gmail.com>
2023-10-17 14:53:27 +1100

    set up git crypt

--- questions_popup.py
+++ questions_popup.py
@@ -1,12 +1,41 @@
 # task scheduled on windows 10 under Questionnaire GUI
-
+import os
 import csv
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 from datetime import datetime
-import os
+
+if 'WSLENV' not in os.environ and os.name == 'nt':
+    # Running on native Windows (not in WSL)
+    import ctypes
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+
+
 
 FILE_NAME = "activities.csv"
+
+
+class CustomSimpleDialog(simpledialog._QueryString):
+
+    def body(self, master):
+        # Removed the line that was causing the error
+
+        label = tk.Label(master,
+                         text=self.prompt,
+                         font='Arial 18',
+                         wraplength=400)
+        label.pack(padx=5, pady=5)
+
+        self.entry = tk.Entry(master, font='Arial 18')
+        self.entry.pack(padx=5, pady=5)
+        self.entry.bind("<Return>", self.ok)
+        self.entry.focus_set()
+
+
+def custom_ask_string(title, prompt):
+    d = CustomSimpleDialog(title=title, prompt=prompt)
+    return d.result
 
 
 def get_last_planned_activity():
@@ -29,20 +58,19 @@ def ask_questions():
     root.withdraw()
 
     current_time_display = datetime.now().strftime('%d/%m %A %I:%M%p')
-
     planned_last_hour = get_last_planned_activity()
 
     if planned_last_hour:
-        past_activity = simpledialog.askstring(
+        past_activity = custom_ask_string(
             "Activity Check",
             f"{current_time_display}\n\nWhat did you do in the last hour? (You planned: {planned_last_hour})"
         )
     else:
-        past_activity = simpledialog.askstring(
+        past_activity = custom_ask_string(
             "Activity Check",
             f"{current_time_display}\n\nWhat did you do in the last hour?")
 
-    next_activity = simpledialog.askstring(
+    next_activity = custom_ask_string(
         "Activity Planning",
         f"{current_time_display}\n\nWhat's your plan for the next hour?")
 
@@ -51,10 +79,14 @@ def ask_questions():
 
 
 def main():
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     past_activity, next_activity = ask_questions()
 
-    with open(FILE_NAME, "a", newline='') as file:
+    # If either of the entries is blank, exit without writing to the CSV
+    if not past_activity or not next_activity:
+        return
+
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open('activity_log.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([current_time, past_activity, next_activity])
 
