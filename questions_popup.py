ef22c2f0847a5194b69b185db4e86155222cfe95
archie3DXR <archie.majumdar@gmail.com>
2023-10-17 14:24:47 +1100

    updated gui and questions

--- questions_popup.py
+++ questions_popup.py
@@ -1,27 +1,50 @@
 import csv
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
+from datetime import datetime
+import os
+
+FILE_NAME = "activities.csv"
+
+def get_last_planned_activity():
+    if not os.path.exists(FILE_NAME):
+        return None
+
+    with open(FILE_NAME, "r", newline='') as file:
+        reader = csv.reader(file)
+        last_line = None
+        for row in reader:
+            last_line = row
+
+    if last_line:
+        return last_line[2]
+    return None
 
 def ask_questions():
     root = tk.Tk()
-    root.withdraw()  # Hide the main window
+    root.withdraw()
 
-    # List of questions
-    questions = ["What is your name?", "How old are you?", "What's your favorite color?"]
+    current_time_display = datetime.now().strftime('%d/%m %A %I:%M%p')
 
-    # Get answers
-    answers = [simpledialog.askstring("Input", q) for q in questions]
+    planned_last_hour = get_last_planned_activity()
 
-    return answers
+    if planned_last_hour:
+        past_activity = simpledialog.askstring("Activity Check", f"{current_time_display}\n\nWhat did you do in the last hour? (You planned: {planned_last_hour})")
+    else:
+        past_activity = simpledialog.askstring("Activity Check", f"{current_time_display}\n\nWhat did you do in the last hour?")
 
-def write_to_csv(answers):
-    with open("answers.csv", "a", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(answers)
+    next_activity = simpledialog.askstring("Activity Planning", f"{current_time_display}\n\nWhat's your plan for the next hour?")
+
+    root.destroy()
+    return past_activity, next_activity
 
 def main():
-    answers = ask_questions()
-    write_to_csv(answers)
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    past_activity, next_activity = ask_questions()
+
+    with open(FILE_NAME, "a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([current_time, past_activity, next_activity])
 
 if __name__ == "__main__":
     main()
